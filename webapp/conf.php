85dd6beb7440bf3d61ec3600696d48d45e6b78ca
Thibaut Marmin <marminthibaut@gmail.com>
2012-10-24 17:30:09 +0200

    add con file

---
+++ webapp/conf.php
@@ -0,0 +1,17 @@
+<?php
+
+define("SPARQL_ENDPOINT", "http://um2opendata.thibautmarmin.fr/sparql");
+define("SPARQL_NS", "PREFIX db: <http://um2opendata.thibautmarmin.fr/resource/>
+PREFIX ev: <http://purl.org/NET/c4dm/event.owl#>
+PREFIX mo: <http://ontology.um2opendata.thibautmarmin.fr#>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX nimp: <http://nimp.com/>
+PREFIX meta: <http://www4.wiwiss.fu-berlin.de/bizer/d2r-server/metadata#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX time: <http://um2opendata.thibautmarmin.fr/resource/neuneu.com>
+PREFIX d2r: <http://sites.wiwiss.fu-berlin.de/suhl/bizer/d2r-server/config.rdf#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX map: <http://um2opendata.thibautmarmin.fr/resource/#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX vocab: <http://um2opendata.thibautmarmin.fr/resource/vocab/>");
