434a58f9538ea6816cef0d2502c25bcce2a49d3c
Thibaut Marmin <marminthibaut@gmail.com>
2012-11-01 00:59:11 +0100

    modif rdfexplorer

--- rdfexplorer/lib/graph.php
+++ rdfexplorer/lib/graph.php
@@ -1,15 +1,16 @@
 <?php
+require_once 'defines.php';
 
 $repo = escapeshellcmd(urldecode($_GET['repo']));
 $input_type = escapeshellcmd(urldecode($_GET['input_type']));
 
 
-$output_type = "png";
-$mime_type = "image/png";
+$output_type = "jpg";
+$mime_type = "image/jpeg";
 
 $rapper_cmd = "rapper -o dot -i $input_type $repo";
 
-$dot_cmd = "dot -T$output_type";
+$dot_cmd = DOTCMD." -T$output_type";
 
 $cmd = $rapper_cmd . ' | ' . $dot_cmd;
 
